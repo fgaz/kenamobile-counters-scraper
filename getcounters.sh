b063f6502d2e43c74a33284679bca8c43599c20b
Francesco Gazzetta <francygazz@gmail.com>
2018-06-29 16:46:25 +0200

    It works! \o/

--- getcounters.sh
+++ getcounters.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
+
+set -e
+
 PHONENUMBER="${1}"
-PHPSESSID=""
-curl -b "PHPSESSID=$PHPSESSID" \
-     -d "action=maya_interrogate&maya_action=getUserPromoBags&msisdn=$PHONENUMBER" \
-     https://www.kenamobile.it/wp-admin/admin-ajax.php | jq
+PASSWORD="${2}"
+cookie=$(curl -s \
+           --cookie-jar - \
+           --output /dev/null \
+           -d "action=maya_interrogate&maya_action=ldapLogin&user=${PHONENUMBER}&userPassword=${PASSWORD}" \
+           https://www.kenamobile.it/wp-admin/admin-ajax.php \
+           | tail -n1)
+phpsessid=$(echo "${cookie}" | awk '{ print $7 }')
+json=$(curl -s \
+         -b "PHPSESSID=${phpsessid}" \
+         -d "action=maya_interrogate&maya_action=getUserPromoBags&msisdn=${PHONENUMBER}" \
+         https://www.kenamobile.it/wp-admin/admin-ajax.php)
+echo "${json}" | jq
 
